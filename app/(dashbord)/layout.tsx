45a8aecc06e934d41970498f7b0acf1018a6645c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-24 14:55:59 -0300

    feat: add LeftSideBar Component

--- app/(dashbord)/layout.tsx
+++ app/(dashbord)/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import LeftSideBar from "@/components/layout/LeftSideBar";
+import TopBar from "@/components/layout/TopBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <div className="flex max-lg:flex-col text-grey-1  ">
+            <LeftSideBar />
+            <TopBar />
+            <div className="flex-1">{children}</div>
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
