4759870d5e0ab739cc32b6007eeb0b52e6c9f8de
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-24 15:00:33 -0300

    fix: clerk version

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <SignUp path="/sign-up" />
+      <SignUp />
     </div>
   );
 }
