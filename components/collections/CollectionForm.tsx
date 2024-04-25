d77fc51a9ba9273fca039c3bb02ded1bb66a2cb2
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-25 15:33:00 -0300

    feat: add Toaster

--- components/collections/CollectionForm.tsx
+++ components/collections/CollectionForm.tsx
@@ -20,6 +20,8 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "../ui/textarea";
 import ImageUpload from "../custom ui/ImageUpload";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
   title: z.string().min(2).max(20),
@@ -29,6 +31,8 @@ const formSchema = z.object({
 
 const CollectionForm = () => {
   const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -39,7 +43,21 @@ const CollectionForm = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      setLoading(true);
+      const res = await fetch("/api/collections", {
+        method: "POST",
+        body: JSON.stringify(values),
+      });
+      if (res.ok) {
+        setLoading(false);
+        toast.success("Collection created");
+        router.push("/collections");
+      }
+    } catch (err) {
+      console.log("[collections_POST]", err);
+      toast.error("Something went wrong! Please try again.");
+    }
   };
   return (
     <div className="p-10">
