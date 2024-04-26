f111ddd11a0987ac6f211819ff5b996ad598f63b
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-26 10:44:56 -0300

    feat: add Delete Collection Button

--- components/custom ui/Delete.tsx
+++ components/custom ui/Delete.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import { Trash } from 'lucide-react'
+import React from "react";
+import { Trash } from "lucide-react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import { Button } from "../ui/button";
 
 const Delete = () => {
   return (
-    <Button className="bg-red-1 text-white">
-        <Trash className="h-4 w-4"/>
-    </Button>
-  )
-}
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button className="bg-red-1 text-white">
+          <Trash className="h-4 w-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="bg-white text-grey-1">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-red-1">
+            Are you absolutely sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete
+            collection.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction className="bg-red-1 text-white">
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
 
-export default Delete
+export default Delete;
