5b6185b51391ca67f9c2ba4398ea52a6a11b2b97
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-04-29 11:13:36 -0300

    feat: add Product Type

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -5,3 +5,19 @@ type CollectionType = {
   image: string;
   products: ProductType[];
 };
+
+type ProductType = {
+  _id: string;
+  title: string;
+  description: string;
+  media: [string];
+  category: string;
+  collection: [CollectionType];
+  tags: [string];
+  sizes: [string];
+  colors: [string];
+  price: number;
+  expense: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
