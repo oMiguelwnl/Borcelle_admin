24b1ae006312491b9ee7c889c00354e0a30d5e46
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-01 17:32:26 -0300

    Product GET request and error handling

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -65,3 +65,18 @@ export const POST = async (req: NextRequest) => {
     return new NextResponse("Internal Error", { status: 500 });
   }
 };
+
+export const GET = async (req: NextRequest) => {
+  try {
+    await connectToDB();
+
+    const products = await Product.find()
+      .sort({ createdAt: "desc" })
+      .populate({ path: "collections", model: Collection });
+
+    return NextResponse.json(products, { status: 200 });
+  } catch (err) {
+    console.log("[products_GET]", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
